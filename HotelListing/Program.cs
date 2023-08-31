427032b45d081a828c62c3182e7f41d63d481e4d
JhayCodes <jhayycodes@gmail.com>
2023-08-31 19:38:44 +0000

    Added custom Cors Policy

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var logger = new LoggerConfiguration();
+
 
 
 var app = builder.Build();
